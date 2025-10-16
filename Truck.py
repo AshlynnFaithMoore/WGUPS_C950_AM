a709e3a4e18143c74f8ae553669e2a0107f61df7
Ashlynn <ashlynnfaith22@gmail.com>
2025-10-15 20:42:24 -0700

    Built out Truck class

--- Truck.py
+++ Truck.py
@@ -1,15 +1,35 @@
 # calculate routes, track mileage, and update delivery times/statuses.
+from datetime import timedelta
 
 class Truck:
-    def __init__(self, self, capacity, speed, load, packages, mileage, address, depart_time):
+    def __init__(self, capacity, speed, load, packages, mileage, address, depart_time):
         self.capacity = capacity
         self.speed = speed
         self.load = load
-        self.packages = packages
-        self.mileage = mileage
+        self.packages = []
+        self.mileage = 0.0 # starting mileage
         self.address = address
         self.depart_time = depart_time
         self.time = depart_time
+
+    def add_package(self, packageID):
+        if len(self.packages) < self.capacity:
+            self.packages.append(packageID)
+        else:
+            print("Package capacity exceeded")
+
+    def update_mileage(self, mileage):
+        travel_time = timedelta(hours= mileage / self.speed)
+        self.time += travel_time
+        self.mileage += mileage
+
+    # to read and debug
+    def __str__(self):
+        return (f"Truck(address={self.address}, mileage={self.mileage:.2f}, "
+                f"time={self.time}, packages={self.packages})")
+
+
+
 
 
 
